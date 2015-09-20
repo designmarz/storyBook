2760672fabc869cb37fb4d4df1a2a9da275d27e9
Nick Marazzo <designmarz@gmail.com>
2015-09-20 16:55:12 -0700

    lots of little updates, working on changing the img src of the background img onClick next

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 /*jslint vars: true, plusplus: true, jquery: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 // /*global define */
-var book = $('.book');
-var chapters = $('.chapter-list');
-// var size = [];
-var height = $('.navbar-hidden').css('height');
+ book = $('.book');
+ chapters = $('.chapter-list');
+ pages = $('.page');
+ list = []
+ size = $('.chapter-list').length;
+ height = $('.navbar-hidden').css('height');
 
 $("<style>").prop("type", "text/css").html(".visible { bottom: " + height + ";}").appendTo("head");
 
@@ -22,19 +24,36 @@ $(".navbar-hidden").click(function(){
         $(".navbar").toggleClass('visible', 500);
     });
 
+// Chapter Section
+$('.navbar-counter').text("Chapter " + size);
 
-// $(".navbar-menu").click(function(){
-// 	$('.visible2').css('bottom', $('.navbar-hidden').css('height'));
-//     });
 
 
 // debugging area, to be removed //
-
+	console.log("------------------------------");
 	for (var i = 0; i < chapters.length; i++) {
 		console.log(chapters[i]);
 	}
+	console.log("------------------------------");
+
+	for (var i = 0; i < pages.length; i++) {
+		var remove = pages[i].outerHTML.length - 5;
+		console.log(pages[i]);
+		console.log(remove);
+		list.push(
+		pages[i].outerHTML.slice(17,remove)
+		);
+		console.log(list);
+	}
+	console.log("------------------------------");
 	// console.log($('.visible').css('bottom'));
+	console.log("Book: ");
 	console.log(book);
+	console.log("Chapters: ");
+	console.log(chapters);
+	console.log("Pages: ");
+	console.log(pages);
+	console.log("------------------------------");
   	console.log("Doc Ready");
 // debugging area, to be removed //
 });
