f3d6c8375e8dce1d913b303f12ff2a07aaf0fada
Nick Marazzo <designmarz@gmail.com>
2015-09-20 17:14:58 -0700

    got button navigaction working, but had to hard code a image src, need a defualt img or something

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
  chapters = $('.chapter-list');
  pages = $('.page');
  list = []
+ whichView = 0;
  size = $('.chapter-list').length;
  height = $('.navbar-hidden').css('height');
 
@@ -27,33 +28,57 @@ $(".navbar-hidden").click(function(){
 // Chapter Section
 $('.navbar-counter').text("Chapter " + size);
 
-
-
-// debugging area, to be removed //
-	console.log("------------------------------");
-	for (var i = 0; i < chapters.length; i++) {
-		console.log(chapters[i]);
-	}
-	console.log("------------------------------");
-
-	for (var i = 0; i < pages.length; i++) {
-		var remove = pages[i].outerHTML.length - 5;
-		console.log(pages[i]);
-		console.log(remove);
+// Build list of images
+for (var i = 0; i < pages.length; i++) {
+	// figure out the length need to grab the whole file path
+	// well need to re-write this when I change to adding a setup page
+	// or upload section
+	var remove = pages[i].outerHTML.length - 5;
 		list.push(
 		pages[i].outerHTML.slice(17,remove)
 		);
-		console.log(list);
+		// console.log(list);
 	}
-	console.log("------------------------------");
-	// console.log($('.visible').css('bottom'));
-	console.log("Book: ");
-	console.log(book);
-	console.log("Chapters: ");
-	console.log(chapters);
-	console.log("Pages: ");
-	console.log(pages);
-	console.log("------------------------------");
+
+
+
+$('#prev-button').on({
+    'click': function(){
+    	whichView -= 1
+        $('#book-img').attr('src',list[whichView]);
+        console.log(whichView);
+        console.log(list[whichView]);
+    }
+});
+
+$('#next-button').on({
+    'click': function(){
+    	whichView += 1
+        $('#book-img').attr('src',list[whichView]);
+        console.log(whichView);
+        console.log(list[whichView]);
+    }
+});
+
+$('#book-img').attr('src',list[whichView]);
+
+// debugging area, to be removed //
+	// console.log("------------------------------");
+	// for (var i = 0; i < chapters.length; i++) {
+	// 	console.log(chapters[i]);
+	// }
+	// console.log("------------------------------");
+
+	
+	// console.log("------------------------------");
+	// // console.log($('.visible').css('bottom'));
+	// console.log("Book: ");
+	// console.log(book);
+	// console.log("Chapters: ");
+	// console.log(chapters);
+	// console.log("Pages: ");
+	// console.log(pages);
+	// console.log("------------------------------");
   	console.log("Doc Ready");
 // debugging area, to be removed //
 });
